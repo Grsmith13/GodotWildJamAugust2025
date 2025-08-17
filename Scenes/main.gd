965c39d48a3844a54326a725751ef634261f02b7
LukeSSJ <LukeSSJ@users.noreply.github.com>
2025-08-17 18:50:54 +0100

    Changed to compat

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,21 +1,19 @@
 class_name Overworld extends Node2D
 
-
 @onready var progress_label = $ProgressLabel
 @onready var nest = $Nest
 
-
 var score = 0;
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var food = get_node("Food")
 
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
 
 
 func _on_food_eaten() -> void:
